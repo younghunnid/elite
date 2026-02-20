563fccda2707f5495477885f04ba2b555a8a6f71
Young Hunnid <younghunnid@Youngs-Mac-mini.local>
2026-02-20 23:40:40 +0000

    fix: update vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/', // ✅ ADD THIS LINE
+    base: '/elite', // ✅ ADD THIS LINE
     server: {
       port: 3000,
       host: '0.0.0.0',
